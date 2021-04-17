5c2616466527647db4fbfbd527985c674294ef14
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-04-17 13:05:25 +0300

    refactor - method extraction

--- src/final.py
+++ src/final.py
@@ -15,6 +15,7 @@ from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score, KFold, GridSearchCV, train_test_split
 from sklearn.utils._testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
+
 features = [0,
             2,
             6,
@@ -54,7 +55,7 @@ def split_to_data_and_target(df: pd.DataFrame):
     return X, y
 
 
-def convert_sars_cov_exam_results_to_binary(df: pd.DataFrame):
+def convert_exam_results_to_binary(df: pd.DataFrame):
     df.loc[df[df.columns[1]] == 'positive', [df.columns[1]]] = 1
     df.loc[df[df.columns[1]] == 'negative', [df.columns[1]]] = 0
 
@@ -78,75 +79,106 @@ def impute(X):
     imputer.fit(X)
     return imputer.transform(X)
 
+
 def preprocessing(df):
     df = project_columns(df)
     df = filter_nulls(df)
-    convert_sars_cov_exam_results_to_binary(df)
+    convert_exam_results_to_binary(df)
     X, y = split_to_data_and_target(df)
     return impute(X), y
 
 
-def main(filename):
-    X, y = preprocessing(filename)
-    print("finish preprocessing")
+# template code taken from https://machinelearningmastery.com/nested-cross-validation-for-machine-learning-with-python/
+def find_best_hyperparams(X, y):
     outer_cv = KFold(n_splits=5)
     best_score = None
-    params = None
+    best_params = None
     for train_xi, test_xi in outer_cv.split(X):
         X_train, X_test = X[train_xi, :], X[test_xi, :]
         y_train, y_test = y[train_xi], y[test_xi]
 
         inner_cv = KFold(n_splits=5)
-
         model = Pipeline([
-            ('over sampling', SVMSMOTE(sampling_strategy=1, k_neighbors=5)),
+            ('over_sampling', SVMSMOTE(sampling_strategy=1, k_neighbors=5)),
             ('classifier', RandomForestClassifier())
         ])
-        # scoring='accuracy'
-        gridCV = GridSearchCV(estimator=model, param_grid=options_random_forest, scoring='accuracy', cv=inner_cv, refit=True)
+        gridCV = GridSearchCV(
+            estimator=model,
+            param_grid=options_random_forest,
+            scoring='f1',
+            cv=inner_cv,
+            refit=True
+        )
 
         result = gridCV.fit(X_train, y_train)
-        # get the best performing model fit on the whole training set
         best_model = result.best_estimator_
-        # evaluate model on the hold out dataset
+        params = result.best_params_
+
         y_predict = best_model.predict(X_test)
-        # evaluate the model
         score = f1_score(y_test, y_predict)
-        # store the result
+
         print("Current model:")
         pprint(result.best_params_)
         print("Score:")
         print(score)
-        if best_score is None:
+
+        if best_score is None or score > best_score:
             best_score = score
-            params = result.best_params_
-        elif score > best_score:
-            best_score = score
-            params = result.best_params_
+            best_params = params
+
     print("Final result:")
-    pprint(params)
+    pprint(best_params)
     print("score:")
     print(best_score)
-    clf = RandomForestClassifier(n_estimators=params.get("classifier__n_estimators"),
-                                 max_depth=params.get("classifier__max_depth"))
+
+    return best_params
+
+
+def calculate_test_metrics(X, y, params):
     results = {"Accuracy": [],
                "F1-score": [],
                "Sensitivity": [],
                "Specificity": [],
                "AUROC": []}
+    retrain(X, y, params, results)
+    normalize_metric_results(results)
+    return results
+
+
+def retrain(X, y, params, results):
     for i in range(1, 11):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= i)
-        model = Pipeline([
-            ('over sampling', SVMSMOTE(sampling_strategy=1, k_neighbors=5)),
-            ('model', clf)
-        ])
-        model.fit(X_train, y_train)
-        prediction = model.predict(X_test)
-        results["Accuracy"].append(accuracy_score(y_test, prediction))
-        results["F1-score"].append(f1_score(y_test, prediction))
-        results["Sensitivity"].append(sensitivity_score(y_test, prediction))
-        results["Specificity"].append(specificity_score(y_test, prediction))
-        results["AUROC"].append(roc_auc_score(y_test, prediction))
+        retrain_iter(X, y, params, results, i)
+
+
+def retrain_iter(X, y, params, results, i):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=i)
+    model = get_retrain_model(params, i)
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    append_scores(results, y_test, prediction)
+
+
+def get_retrain_model(params, i):
+    clf = RandomForestClassifier(
+        n_estimators=params.get("classifier__n_estimators"),
+        max_depth=params.get("classifier__max_depth")
+    )
+    model = Pipeline([
+        ('over sampling', SVMSMOTE(sampling_strategy=1, k_neighbors=5)),
+        ('model', clf)
+    ])
+    return model
+
+
+def append_scores(results, y_test, prediction):
+    results["Accuracy"].append(accuracy_score(y_test, prediction))
+    results["F1-score"].append(f1_score(y_test, prediction))
+    results["Sensitivity"].append(sensitivity_score(y_test, prediction))
+    results["Specificity"].append(specificity_score(y_test, prediction))
+    results["AUROC"].append(roc_auc_score(y_test, prediction))
+
+
+def normalize_metric_results(results):
     temp = np.array(results["Accuracy"])
     results["Accuracy"] = np.mean(temp), np.std(temp)
     temp = np.array(results["F1-score"])
@@ -157,7 +189,15 @@ def main(filename):
     results["Specificity"] = np.mean(temp), np.std(temp)
     temp = np.array(results["AUROC"])
     results["AUROC"] = np.mean(temp), np.std(temp)
+
+
+def main(filename):
+    X, y = preprocessing(filename)
+    print("finish preprocessing")
+    params = find_best_hyperparams(X, y)
+    results = calculate_test_metrics(X, y, params)
     pprint(results)
+
 
 if __name__ == '__main__':
     main("./dataset.csv")
