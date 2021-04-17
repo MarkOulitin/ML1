f3ea700ce22905cafd4e48a16f69b5e5ad43cf12
Mark Oulitin <fragoric@gmail.com>
2021-04-17 10:03:52 +0300

    inner cv in work

--- src/final.py
+++ src/final.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from pprint import pprint
+
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
@@ -5,7 +8,7 @@ from numpy import mean, std
 
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
-from sklearn.pipeline import Pipeline
+from imblearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score, KFold, GridSearchCV
 from sklearn.utils._testing import ignore_warnings
@@ -36,8 +39,8 @@ features = [0,
             48,
             47]
 
-options_random_forest = {"n_estimators": [*range(10, 40, 10)] + [*range(45, 105, 5)],
-                         "max_depth": [2 ** i for i in range(1, 7)]}
+options_random_forest = {'n_estimators': list(range(10, 40, 10)) + list(range(45, 105, 5)),
+                         'max_depth': [2 ** i for i in range(1, 7)]}
 options_xgboost = {"learning_rate": [0.01, 0.05, 0.1]}
 
 
@@ -81,12 +84,28 @@ def main(filename):
     X, y = preprocessing(filename)
     # pipeline = Pipeline(steps=[('i', IterativeImputer()), ('m', RandomForestClassifier())])
     # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
     inner_cv = KFold(n_splits=5)
-    outer_cv = KFold(n_splits=5)
-    clf = GridSearchCV(estimator= RandomForestClassifier(), param_grid=options_random_forest, cv=inner_cv)
-    nested_score = cross_val_score(clf, X=X, y=y, cv=outer_cv)
-    print(nested_score)
+    model = Pipeline([
+        ('over sampling', SMOTE(sampling_strategy=1, k_neighbors=5)),
+        ('classifier', RandomForestClassifier())
+    ])
+    gridCV = GridSearchCV(estimator= model, param_grid=options_random_forest, cv=inner_cv)
+    pprint(model.get_params())
+    # outer_cv = KFold(n_splits=5)
+    # scores = cross_val_score(gridCV, X=X, y=y, scoring='f1', cv=outer_cv)
+    # print(scores)
 
 
 if __name__ == '__main__':
     main("./dataset.csv")
+
+
+# inner_cv = KFold(n_splits=5)
+#     over = SMOTE(sampling_strategy=1, k_neighbors=5)
+#     model = GridSearchCV(estimator= RandomForestClassifier(), param_grid=options_random_forest, cv=inner_cv)
+#     inner_steps = [('over', over), ('model', model)]
+#     pipeline = Pipeline(steps=inner_steps)
+#     outer_cv = KFold(n_splits=5)
+#     scores = cross_val_score(pipeline, X=X, y=y, scoring='f1', cv=outer_cv)
+#     print(scores)
